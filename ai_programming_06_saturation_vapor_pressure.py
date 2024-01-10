5450cfe1a16dd2119dab4fda3a90b7e526bba9e3
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2024-01-10 10:04:32 +0800

    Create ai_programming_06_saturation_vapor_pressure.py

---
+++ ai_programming_06_saturation_vapor_pressure.py
@@ -0,0 +1,27 @@
+"""
+File     : ai_programming_06_saturation_vapor_pressure.py
+Title    : AI與程式語言-第6章 saturation vapor pressure
+Author   : Ming-Chang Lee
+Email    : alan9956@gmail.com
+RWEPA    : http://rwepa.blogspot.tw/
+GitHub   : https://github.com/rwepa
+Encoding : UTF-8
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 範例1
+T = 50
+es = 611*np.exp(17.27*T/(237.3+T))
+print(es)
+
+# 範例2
+T = np.linspace(-100,100,50)
+es = 611*np.exp(17.27*T/(237.3+T))
+
+plt.plot(T,es)
+plt.xlabel('T (degree Celcius)')
+plt.ylabel('es (Pa)')
+plt.title('tku-LEE Ming-Chang')
+# end
