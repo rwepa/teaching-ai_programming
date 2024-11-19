59bff50d52cab71a263ee3cf13f64832adc82dd8
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2024-11-19 22:01:40 +0800

    Update ai_programming_06_saturation_vapor_pressure.py

--- ai_programming_06_saturation_vapor_pressure.py
+++ ai_programming_06_saturation_vapor_pressure.py
@@ -1,6 +1,6 @@
 """
 File     : ai_programming_06_saturation_vapor_pressure.py
-Title    : AI與程式語言-第6章 saturation vapor pressure (飽和水氣壓曲線)
+Title    : AI與程式語言-第6章 saturation vapor pressure
 Author   : Ming-Chang Lee
 Email    : alan9956@gmail.com
 RWEPA    : http://rwepa.blogspot.tw/
@@ -8,20 +8,32 @@ GitHub   : https://github.com/rwepa
 Encoding : UTF-8
 """
 
+# saturation vapor pressure (飽和水氣壓)
+# https://en.wikipedia.org/wiki/Tetens_equation
+# https://en.wikipedia.org/wiki/Vapour_pressure_of_water
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib import rcParams # 載入繪圖參數設定
+rcParams['font.sans-serif'] = ['Microsoft YaHei'] # 設定字型
+rcParams['axes.unicode_minus'] = False # 設定負號正確顯示
+
 # 範例1
+# 飽和水氣壓計算
 T = 50
-es = 611*np.exp(17.27*T/(237.3+T))
+a = 17.27
+b = 237.3
+es = 0.61078*np.exp(a*T/(T+b))
 print(es)
 
 # 範例2
+# 飽和水氣壓曲線繪圖
 T = np.linspace(-100,100,50)
-es = 611*np.exp(17.27*T/(237.3+T))
-
-plt.plot(T,es)
+es_formula = 0.61078*np.exp(a*T/(T+b))
+plt.plot(T, es_formula)
 plt.xlabel('T (degree Celcius)')
 plt.ylabel('es (Pa)')
-plt.title('tku-LEE Ming-Chang')
+plt.title('TKU-168-李明昌')
+plt.show()
 # end
